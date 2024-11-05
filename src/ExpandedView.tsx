808d28ef55b6e0a49106e3732449d92f1cdfc5b5
adrozd <anton.drozd.95@gmail.com>
2024-11-05 15:48:51 +0200

    remove google apis

--- src/ExpandedView.tsx
+++ src/ExpandedView.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import {
   Box,
   Table,
@@ -7,14 +6,9 @@ import {
   TableHead,
   TableRow,
   TextField,
-  IconButton,
-  CircularProgress,
 } from "@mui/material";
-import { Translate } from "@mui/icons-material";
 import { languages } from "./data";
 import { LocalizationRow } from "./types";
-
-const googleApiKey = import.meta.env.VITE_GOOGLE_API_KEY;
 
 type Props = {
   row: Pick<
@@ -25,43 +19,11 @@ type Props = {
   onUpdateTranslation: (language: string, translation: string) => void;
 };
 
-const translateText = async (text: string, targetLanguage: string) => {
-  const response = await fetch(
-    `https://translation.googleapis.com/language/translate/v2?key=${googleApiKey}`,
-    {
-      method: "POST",
-      body: JSON.stringify({
-        q: text,
-        target: targetLanguage,
-        source: "en",
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
-  const result = await response.json();
-
-  return result?.data?.translations[0]?.translatedText || "";
-};
-
 const ExpandedView: React.FC<Props> = ({
   row,
   isEditing,
   onUpdateTranslation,
 }) => {
-  const [loadingTranslation, setLoadingTranslation] = useState<boolean>(false);
-
-  const handleTranslate = async (languageCode: string) => {
-    setLoadingTranslation(true);
-    const translatedText = await translateText(
-      row.defaultTranslation,
-      languageCode
-    );
-    onUpdateTranslation(languageCode, translatedText);
-    setLoadingTranslation(false);
-  };
-
   return (
     <Box margin={2}>
       <Table size="small">
@@ -69,7 +31,6 @@ const ExpandedView: React.FC<Props> = ({
           <TableRow>
             <TableCell sx={{ fontWeight: 700 }}>Language</TableCell>
             <TableCell sx={{ fontWeight: 700 }}>Translation</TableCell>
-            <TableCell sx={{ fontWeight: 700 }} />
           </TableRow>
         </TableHead>
         <TableBody>
@@ -88,21 +49,6 @@ const ExpandedView: React.FC<Props> = ({
                   />
                 ) : (
                   <Box>{row.translations[lang.code] || "❌😢"}</Box>
-                )}
-              </TableCell>
-              <TableCell>
-                {isEditing && (
-                  <IconButton
-                    size="small"
-                    onClick={() => handleTranslate(lang.code)}
-                    disabled={loadingTranslation}
-                  >
-                    {loadingTranslation ? (
-                      <CircularProgress size={20} />
-                    ) : (
-                      <Translate color="primary" />
-                    )}
-                  </IconButton>
                 )}
               </TableCell>
             </TableRow>
